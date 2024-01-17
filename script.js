4df611d4d9cd60f760ab738c3d5d825fd38e0b49
Simon Hajduk <simonhajduk@icloud.com>
2024-01-16 19:35:56 -0800

    added a new game

--- script.js
+++ script.js
@@ -7,15 +7,27 @@ NumberBox.addEventListener('change', checkNumber);
 
 function checkNumber() {
     const guess = NumberBox.value;
+  //  let currentTop = Number(NumberBox.style.top.replace('px', ''));
     if (guess < newNumber) {
         FeedbackBox.innerText = 'Too low';
-        NumberBox.style.top = `${newNumber - guess}px`;
+      //  NumberBox.style.top = `${currentTop - newNumber - guess}px`;
     }
     else if (guess > newNumber) {
         FeedbackBox.innerText = 'Too high';
-        NumberBox.style.top = `${guess - newNumber}px`;
+     //   NumberBox.style.top = `${currentTop + guess - newNumber}px`;
     }
     else {
         FeedbackBox.innerText = 'You got it!';
     }
+}
+
+const inputSquare = document.getElementById('sqin');
+const outputSquare = document.getElementById('sqout');
+
+
+inputSquare.addEventListener('mouseenter', Sqcheck);
+
+function Sqcheck() {
+    const sqguess = inputSquare.value;
+    outputSquare.innerText = sqguess**2;
 }
